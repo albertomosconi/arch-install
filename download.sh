2a9f62c282628200995fa75a662aa1b943806c82
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-03-26 01:19:11 +0100

    add artix script to download script

--- download.sh
+++ download.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 rm -f base-uefi.sh
+rm -f artix.sh
 rm -f uefi.conf
 
 curl -O https://raw.githubusercontent.com/albertomosconi/arch-install/main/base-uefi.sh
+curl -O https://raw.githubusercontent.com/albertomosconi/arch-install/main/artix.sh
 curl -O https://raw.githubusercontent.com/albertomosconi/arch-install/main/uefi.conf
 
 chmod +x base-uefi.sh
+chmod +x artix.sh
